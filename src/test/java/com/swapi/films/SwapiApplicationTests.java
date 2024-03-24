fc943f12e35795b152e201f179eca6c22023ee88
Paloma Alves <paloma_passere@hotmail.com>
2024-03-24 14:11:11 -0300

    Configuracao Swagger

--- src/test/java/com/swapi/films/SwapiApplicationTests.java
+++ src/test/java/com/swapi/films/SwapiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.swapi.films;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SwapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
